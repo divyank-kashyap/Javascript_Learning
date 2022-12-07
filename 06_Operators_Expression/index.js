8d4d4cc529a9707b2f64d3e7bdd0fab6f8bc7a51
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2022-12-08 00:25:17 +0530

    Add files via upload
    Operators & Expression

---
+++ 06_Operators_Expression/index.js
@@ -0,0 +1,53 @@
+// 55;      
+// "Divyank Kashyap";
+// false;
+
+console.log("Operators in Js")
+// Arithmatic operators 
+let a = 10;
+let b = 4;
+console.log("a + b = ", a+b)
+console.log("a - b = ", a-b)
+console.log("a * b = ", a*b)
+console.log("a / b = ", a/b)
+console.log("a ** b = ", a**b)
+console.log("++a = ", ++a)  // 11
+console.log("a++ = ", a++)  // 11   // 12
+console.log("--a = ", --a)  // 11
+console.log("a-- = ", a--)  // 11   // 10
+console.log("a = ", a)      // 10
+console.log("a-- = ", a--)  // 10   // 9
+
+
+// Assignment operator 
+let x = 1
+x += 5 // sam as x = x + 5
+console.log(x)
+
+
+// Comparison Operators 
+let comp1 = 6;
+let comp2 = "6";
+console.log("comp1 == comp2 is ", comp1 == comp2)
+console.log("comp1 != comp2 is ", comp1 != comp2)
+console.log("comp1 === comp2 is ", comp1 === comp2)
+console.log("comp1 !== comp2 is ", comp1 !== comp2)
+
+
+// Logical Operators
+let x1 = 5;
+let y1 = 6;
+console.log(x1<y1 && x1==5)
+console.log(x1>y1 || x1==5)
+console.log(!false)
+
+/* This is 
+Multiline 
+Comment
+*/
+
+// This is Single comment
+
+
+
+
