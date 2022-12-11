512028ef798a817eee79d1589e3032426e0fed35
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2022-12-11 21:39:05 +0530

    Add files via upload
    Practise set questions

---
+++ 08_Practise_set/index.js
@@ -0,0 +1,39 @@
+// Q1 : Use logical operators to fund whether the age of a person lies between 10 and 20
+// let age = prompt("What is your age? ");
+// if (age>10 && age<=20){
+//     console.log("Your age lies between 10 and 20")
+// }
+// else{
+//     console.log("Your age doesnt lies betweem 10 and 20")
+// }
+
+//  Q2 : Demonstrate the use of switch case statements in Javascript
+let age = prompt("What is your age? ");
+switch (age){
+    case '12':
+        console.log("Your age is 12")
+        break
+    case '13':
+        console.log("Your age is 13")
+        break
+    case '14':
+        console.log("Your age is 14")
+        break
+    case '15':
+        console.log("Your age is 15")
+        break
+    default:
+        console.log("Your age is not special")
+}
+
+
+
+//  Q3 : Write a Javascript program to find whether a number is divisble by 2 and 3 
+
+
+
+//  Q4 : Write a Javascript program to find whether a number is divisible by either 2 or 3
+
+
+
+//  Q5 : Print "You can Drive " or "You cannot Drive" based on age being greater than 18 using ternary operator 
