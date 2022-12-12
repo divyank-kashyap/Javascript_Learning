109f1f512118f2bc1ced332e2d9f527dba2370eb
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2022-12-12 22:43:24 +0530

    Update index.js

--- 09_For_Loops/index.js
+++ 09_For_Loops/index.js
@@ -1,27 +1,25 @@
-
-
-
+// Program to print sum upto n natural numbers 
 let sum = 0
 let n =  prompt("Enter number : ")
-for (let i = 0; i<=n; i++){
-    sum += i
+for (var i = 0; i<=n; i++){  // let has block level scope
+    sum += i                 // but var has global scope
 }
 console.log(sum)
 console.log(i)
 
 
 // For in loop
-// let obj = {
-//     harry : 90,
-//     shubh : 45,
-//     shivika : 56,
-//     ritika : 57,
-//     shiv : 23
-//   }
+let obj = {
+    harry : 90,
+    shubh : 45,
+    shivika : 56,
+    ritika : 57,
+    shiv : 23
+  }
   
-//   for (let a in obj){
-//     console.log("Marks of " + a + " are " + obj[a])
-//   }
+  for (let a in obj){
+    console.log("Marks of " + a + " are " + obj[a])
+  }
 
 // For of loop
 
