3e167c4f6141f834c4cc7aba167ccf7bb42b3f18
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2022-12-14 00:04:04 +0530

    Add files via upload
    Strings 
    single cot
    double cot
    `` back tik
    Escape Sequences

---
+++ 13_Strings/index.js
@@ -0,0 +1,30 @@
+let name = "Harry"  // double cot
+console.log(name)
+console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+
+let friend = 'Prakash'
+console.log(friend)
+console.log(friend.length)  // single cot
+
+// Template Literals 
+let boy1 = "Pramod"
+let boy2 = "Nikhil"
+// Nikhil is a friend of Pramod
+
+let sentence = `boy1 is a friend of boy2`   // backtik can be used to make string in js
+let sentence2 = `${boy1} is a friend of ${boy2}`   
+console.log(sentence2)
+
+
+// Escape Sequence Characters
+let fruit = 'Bana\'na'
+console.log(fruit)
+console.log(fruit.length)
+
+let fruit2 = 'Bana\nna'
+console.log(fruit2)
+
+let fruit3 = 'Bana\rna'
+console.log(fruit3)
