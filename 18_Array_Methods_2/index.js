bcda97c19c325d623d373bce78e510ab2483748d
Divyank <64241054+divyank-kashyap@users.noreply.github.com>
2022-12-17 18:28:07 +0530

    Add files via upload
    Array methods
    sort
    splice
    slice
    reverse

---
+++ 18_Array_Methods_2/index.js
@@ -0,0 +1,30 @@
+// // 
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let num_more = [11, 12, 13, 14, 15, 16, 17, 18, 19]
+// let num_even_more = [511, 512,513, 514, 515, 516, 517, 518, 519]
+// // delete num[0]
+// // console.log(num.length)
+// // console.log(num)
+// // console.log(num.length)
+
+// let newArray = num.concat(num_more, num_even_more)
+// console.log(newArray)
+// console.log(num , newArray)
+
+
+
+// Sort Method
+// let compare = (a, b)=>{
+//     return a - b
+// }
+// let num = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+// num.sort(compare)
+// num.reverse()
+// console.log(num)
+
+let num = [551, 22, 3, 14, 5, 6, 7, 8, 229]
+// let deleetedValues = num.splice(2, 3, 1021, 1022, 1023, 1024, 1025)
+// console.log(deleetedValues)
+
+let newNum = num.slice(3, 5)
+console.log(newNum)
---
+++ 18_Array_Methods_2/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+delete num[0]
+// console.log(num.length)
+// console.log(num)
+// console.log(num.length)
